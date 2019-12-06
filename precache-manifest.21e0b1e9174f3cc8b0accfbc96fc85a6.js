ede7cb51e95bafd5bb8cd7728155048a7982b8dc
AleksandrKaradzikov <karadzikovaleksandr@gmail.com>
2019-12-06 17:26:45 +0300

    Updates

---
+++ precache-manifest.21e0b1e9174f3cc8b0accfbc96fc85a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8636945f7b4f2ce0890e729517fcadc",
+    "url": "/countdown-timer/index.html"
+  },
+  {
+    "revision": "3fe7f708811be81f770e",
+    "url": "/countdown-timer/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "3e2123833078db53deed",
+    "url": "/countdown-timer/static/css/main.7ebccdf5.chunk.css"
+  },
+  {
+    "revision": "3fe7f708811be81f770e",
+    "url": "/countdown-timer/static/js/2.4d71e7ea.chunk.js"
+  },
+  {
+    "revision": "3e2123833078db53deed",
+    "url": "/countdown-timer/static/js/main.3bc8bbf4.chunk.js"
+  },
+  {
+    "revision": "7863e036ed9056a3ba0a",
+    "url": "/countdown-timer/static/js/runtime-main.0ddd81b6.js"
+  },
+  {
+    "revision": "ff93cfed5bdc6eca7169ab3c94ae5cb7",
+    "url": "/countdown-timer/static/media/finish-sound.ff93cfed.mp3"
+  }
+]);
